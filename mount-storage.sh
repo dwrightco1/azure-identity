eb033778ca466dfcee58282abedd66582fa58b7b
Dan Wright <dwrightco1@gmail.com>
2025-02-06 08:15:43 -0500

    add mount-storage.sh

---
+++ mount-storage.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+usage() {
+  echo "usage: $0 <mid-clientid> <storageacct-name> <share-name> <mountpoint> <resource-group>"
+  exit 1
+}
+
+assert() {
+  echo "ERROR: ${1}"
+  exit 1
+}
+
+next_log() {
+	cnt=0
+	log_path=/tmp/mount-storageacct.$$-${cnt}.log
+	while [ -f ${log_path} ]; do
+		((cnt++))
+		log_path=/tmp/mount-storageacct.$$-${cnt}.log
+	done
+
+	echo "${log_path}"
+}
+
+run_cmd() {
+	if [ $# -ne 2 ]; then return 0; fi
+	title=${1}
+	cmd=${2}
+
+	log=$(next_log)
+	echo -n "--> ${title}"
+	echo "--> Executing: ${cmd} " > ${log} 2>&1
+	eval ${cmd} >> ${log} 2>&1
+	if [ $? -ne 0 ]; then
+		echo " (failed | log = ${log})"
+		return 1
+	fi
+	echo " (succeeded | log = ${log})"
+	return 0
+}
+
+# validate commandline args
+if [ $# -ne 5 ]; then usage; fi
+
+# process commandline args
+clientid=${1}
+storageacct=${2}
+share=${3}
+mountpoint=${4}
+rg=${5}
+
+# login to Azure using managed identity
+run_cmd "az login --identity --client-id ${clientid}"
+if [ $? -ne 0 ]; then assert "failed to login to Azure"; fi
+
+
+# lookup storage account access key
+accesskey=$(az storage account keys list --account-name ${storageacct} --resource-group ${rg} --query [0].value)
+if [ $? -ne 0 ]; then assert "failed to get access key for storage account"; fi
+
+# install cifs utilities
+run_cmd "sudo yum install -y cifs-utils"
+if [ $? -ne 0 ]; then assert "failed to install cifs utilities"; fi
+
+# mount storage account share
+run_cmd "sudo mount -t cifs //${storageacct}.file.core.windows.net/${share} <mountpoint> -o username=${storageacct} -o password=${accesskey}"
+if [ $? -ne 0 ]; then assert "failed to mount storage account share"; fi
+
+# exit cleanly
+exit 0
