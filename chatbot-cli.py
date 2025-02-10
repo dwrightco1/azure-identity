d9a0d2d07dd74471f068c163b3a81e6f9cbafe0f
Dan Wright <dwrightco1@gmail.com>
2025-02-10 10:55:06 -0500

    update chatbot-cli.py

--- chatbot-cli.py
+++ chatbot-cli.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 import sys, os, os.path, base64
 from openai import AzureOpenAI
-from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 import argparse
-from pprint import pprint
-
-# ./chatbot-cli.py -c 6dc22f88-e3c6-4e28-9318-0b4b20c0d746 -e https://eastus.api.cognitive.microsoft.com/ -a 2024-05-01-preview -d gpt-35-turbo
 
 def _parse_args():
     ap = argparse.ArgumentParser(sys.argv[0], formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     ap.add_argument("--clientid", "-c",  help="-c <client-id>", required=True, nargs=1)
+    ap.add_argument("--vault", "-v",  help="-v <vault-url>", required=True, nargs=1)
+    ap.add_argument("--secret", "-s",  help="-s <secret-name>", required=True, nargs=1)
     ap.add_argument("--endpointurl", "-e",  help="-e <endpoint-url>", required=True, nargs=1)
     ap.add_argument("--apiversion", "-a",  help="-a <api-version>", required=True, nargs=1)
     ap.add_argument("--deployment", "-d",  help="-d <deployment>", required=True, nargs=1)
     return ap.parse_args()
 
+def get_secret():
+    credential = DefaultAzureCredential(managed_identity_client_id=args.clientid[0])
+    client = SecretClient(vault_url=args.vault[0], credential=credential)
+
+    secret = client.get_secret(args.secret[0])
+    secret_value = secret.value
+    sys.stdout.write("secret value = {}\n".format(secret_value))
+
 args = _parse_args()
 
-token_provider = get_bearer_token_provider(  
-    DefaultAzureCredential(managed_identity_client_id=args.clientid[0]),  
-    #DefaultAzureCredential(),
-    "https://cognitiveservices.azure.com/.default"  
-)
+subscription_key = "2f64f1dfa5c948ac99ac7b8d0c43b40f"
 
-pprint(vars(token_provider))
-
-# Initialize Azure OpenAI client with key-based authentication
-client = AzureOpenAI(  
-    azure_endpoint=args.endpointurl[0],  
-    azure_ad_token_provider=token_provider,  
+# Initialize OpenAI session
+client = AzureOpenAI(
+    azure_endpoint=args.endpointurl[0],
+    api_key=subscription_key,
     api_version=args.apiversion[0],
-)  
+)
 
 def chat_with_gpt(prompt):
     response = client.chat.completions.create(
@@ -42,6 +44,13 @@ def chat_with_gpt(prompt):
     return(response.choices[0].message.content.strip())
 
 if __name__ == "__main__":
+#    try:
+    get_secret()
+#    except Exception as e:
+#        print(f"Error retrieving OpenAI access key from key vault: {e}")
+#        sys.exit(1)
+
+    # Start chatbot prompt loop
     while True:
         user_input = input("Ask a question: ")
         if user_input.lower() in ["quit","q"]:
